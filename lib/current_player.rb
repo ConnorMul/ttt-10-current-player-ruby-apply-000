e9908861ac1d01bff41cbf0c20287ac5a75c760e
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-13 23:03:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,12 @@ def turn_count(board)
   end
  return number_of_turns
 end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    puts "It is X's turn"
+  else 
+    puts "It is O's turn"
+  end
+  return board
+end 
