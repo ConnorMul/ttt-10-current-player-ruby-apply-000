24d8a1e44ff4f73724db9e2b49c2fa0f3bce4039
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-13 22:53:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   number_of_turns = 0
   board.each do |turn|
     if number_of_turns <= 9
-    numer_of_turns += 1
+     number_of_turns += 1 
    end
   end
  return number_of_turns
